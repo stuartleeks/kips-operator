ac208c4113f0f60ef8e1a4d2327e1d8d9d1760fc
Stuart Leeks <stuart@leeks.net>
2019-10-26 22:18:40 +0100

    explore metadata

--- controllers/servicebridge_controller.go
+++ controllers/servicebridge_controller.go
@@ -2,12 +2,16 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+
+	corev1 "k8s.io/api/core/v1"
 
 	kipsv1alpha1 "faux.ninja/kips-operator/api/v1alpha1"
 )
@@ -26,25 +30,45 @@ func (r *ServiceBridgeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, erro
 	ctx := context.Background()
 	log := r.Log.WithValues("servicebridge", req.NamespacedName)
 
-	// var serviceBridge kipsv1alpha1.ServiceBridge
 	serviceBridge := kipsv1alpha1.ServiceBridge{}
 
 	if err := r.Get(ctx, req.NamespacedName, &serviceBridge); err != nil {
-		err2 := ignoreNotFound(err)
-		if err2 == nil {
-			log.Info("ServiceBridge not found")
-			return ctrl.Result{}, nil
-		}
-		log.Error(err2, "unable to fetch ServiceBridge")
-		return ctrl.Result{}, ignoreNotFound(err2)
+		log.Error(err, "unable to fetch ServiceBridge")
+		return ctrl.Result{}, ignoreNotFound(err)
 	}
 
-	serviceBridge.Status.Temp = "working..." + serviceBridge.Spec.TargetServiceName
+	// TODO - add a finalizer to revert the selectors on the Service: https://book.kubebuilder.io/reference/using-finalizers.html?highlight=delete#using-finalizers
+
+	serviceName := serviceBridge.Spec.TargetServiceName
+	serviceNamespacedName := types.NamespacedName{
+		Namespace: req.Namespace,
+		Name:      serviceName,
+	}
+
+	service := corev1.Service{}
+
+	if err := r.Get(ctx, serviceNamespacedName, &service); err != nil {
+		log.Error(err, "unable to fetch service")
+		return ctrl.Result{}, ignoreNotFound(err)
+	}
+
+	serviceBridge.Status.Temp = fmt.Sprintf("working... %s: %v", serviceBridge.Spec.TargetServiceName, service.Spec.Selector)
 
 	if err := r.Status().Update(ctx, &serviceBridge); err != nil {
 		log.Error(err, "unable to update ServiceBridge status")
 		return ctrl.Result{}, err
 	}
+
+	// TODO store selectors as JSON on the *Service* rather than go map output on *ServiceBridge*
+	serviceBridge.ObjectMeta.Annotations["OriginalSelectors"] = fmt.Sprintf("%v", service.Spec.Selector)
+	if err := r.Update(ctx, &serviceBridge); err != nil {
+		log.Error(err, "unable to update ServiceBridge metadata")
+		return ctrl.Result{}, err
+	}
+
+	// TODO - modify the Service to add the original selectors and apply new ones (Add a check that the original selectors aren't already set, which would indicate another ServiceBridge is already applied - fail in this case)
+	// TODO - create a config map for the azbridge config
+	// TODO - create a deployment for azbridge 
 
 	return ctrl.Result{}, nil
 }
